9587b0a8c1ce013948552f53b1f994c055a8950f
ksb-hub <61408221+ksb-hub@users.noreply.github.com>
2024-07-25 18:00:54 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Docker 이미지 이름과 태그 설정
+IMAGE_NAME="gayun5313/stretching:latest"
+
+# 새로운 이미지 가져오기
+docker pull $IMAGE_NAME
+
+# 기존 컨테이너 중지 및 삭제
+docker stop stretching-container || true
+docker rm stretching-container || true
+
+# 새로운 이미지로 컨테이너 실행
+docker run -d --name stretching-container -p 3005:3005 $IMAGE_NAME
