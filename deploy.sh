2d71d021e567de7a41b77f99ce5be8c2fb16c191
ksb-hub <61408221+ksb-hub@users.noreply.github.com>
2024-07-25 19:26:17 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-docker pull myapp:latest
-docker stop myapp || true
-docker rm myapp || true
-docker run -d --name myapp -p 80:80 myapp:latest
+
+# Docker 이미지 이름과 태그 설정
+IMAGE_NAME="gayun5313/stretching:latest"
+
+# Docker Hub에 로그인 (비상호작용 모드)
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+# 새로운 이미지 가져오기
+docker pull $IMAGE_NAME
+
+# 기존 컨테이너 중지 및 삭제
+docker stop stretching-container || true
+docker rm stretching-container || true
+
+# 새로운 이미지로 컨테이너 실행
+docker run -d --name stretching-container -p 3005:3005 $IMAGE_NAME
