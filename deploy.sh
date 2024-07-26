be0af858dae7535c41fc700ba88452285d0fbbf6
ksb-hub <61408221+ksb-hub@users.noreply.github.com>
2024-07-26 13:06:13 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,49 +1,28 @@
 #!/bin/bash
 
-# Docker 이미지 이름과 태그 설정
-IMAGE_NAME="gayun5313/stretching:latest"
+set -e
 
-# Docker Hub 로그인 정보 파일 소스
-echo "Sourcing Docker credentials..."
-source /tmp/docker_creds.sh
+# Docker Compose 파일의 위치
+DOCKER_COMPOSE_FILE="/home/hermi/deploy/docker-compose.yml"
 
-# 로그인 정보 확인 (디버깅 목적)
-echo "Docker Hub Username: $DOCKER_USERNAME"
+# Docker 로그인 정보
+DOCKER_USERNAME="${DOCKER_USERNAME}"
+DOCKER_PASSWORD="${DOCKER_PASSWORD}"
 
-# Docker Hub에 로그인 (비상호작용 모드)
+# Docker Hub 로그인
 echo "Logging in to Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-# 로그인 오류 확인
-if [ $? -ne 0 ]; then
-    echo "Error: Docker Hub login failed."
-    exit 1
-fi
-echo "Docker Hub login successful."
-
 # 새로운 이미지 가져오기
 echo "Pulling the latest Docker image..."
-docker pull $IMAGE_NAME
-
-# 이미지 풀링 오류 확인
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to pull image $IMAGE_NAME."
-    exit 1
-fi
-echo "Docker image pulled successfully."
+docker pull gayun5313/stretching:latest
 
 # 기존 컨테이너 중지 및 삭제
-docker stop stretching-container || true
-docker rm stretching-container || true
+echo "Stopping and removing existing container..."
+docker-compose -f "$DOCKER_COMPOSE_FILE" down || true
 
-# 새로운 이미지로 컨테이너 실행
+# 새로운 컨테이너 시작
 echo "Starting new Docker container..."
-docker run --env-file /tmp/.env -d --name stretching-container -p 3005:3005 $IMAGE_NAME
-
-# 컨테이너 실행 오류 확인
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to start container stretching-container."
-    exit 1
-fi
+docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
 
 echo "Deployment completed successfully."
