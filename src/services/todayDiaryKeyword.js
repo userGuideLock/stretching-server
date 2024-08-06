b15b9dfc7c28e4ad78a5529bf9ae365390cec56c
Your Name <kim946509@gmail.com>
2024-08-06 20:19:59 +0900

    feat: get diary and keyword

--- src/services/todayDiaryKeyword.js
+++ src/services/todayDiaryKeyword.js
@@ -3,9 +3,11 @@ const db = require('../models');
 // 클라이언트로부터 userId와 date를 받고 해당 날짜의 다이어리와 키워드를 반환
 const getTodayDiaryKeyword = async (req, res) => {
   const userId = req.params.userId;
-  const date = req.query.date;
+  const lowestDate = req.query.lowestdate;
+  const highestDate = req.query.highestdate;
+  const lowestSlopeDate = req.query.lowestdate;
 
-  if (!userId || !date) {
+  if (!userId || !lowestDate || !highestDate || !lowestDate) {
     return res.status(400).json({ code: 1, message: 'Invalid input' });
   }
 
@@ -16,12 +18,43 @@ const getTodayDiaryKeyword = async (req, res) => {
   }
 
   //해당 날짜 다이어리 조회
-  const diary = await db.Diary.findOne({ where: { userId, date } });
+  const lowestDateDiary = await db.Diary.findOne({
+    where: { userId, date: lowestDate },
+  });
 
   //해당 날짜 키워드 조회
-  const keyword = await db.Keyword.findOne({ where: { userId, date } });
+  const lowestDateKeyword = await db.Keyword.findOne({
+    where: { userId, date: lowestDate },
+  });
 
-  return res.status(200).json({ diary, keyword });
+  //해당 날짜 다이어리 조회
+  const highestDateDiary = await db.Diary.findOne({
+    where: { userId, date: highestDate },
+  });
+
+  //해당 날짜 키워드 조회
+  const highestDateKeyword = await db.Keyword.findOne({
+    where: { userId, date: highestDate },
+  });
+
+  //해당 날짜 다이어리 조회
+  const lowestSlopeDateDiary = await db.Diary.findOne({
+    where: { userId, date: lowestSlopeDate },
+  });
+
+  //해당 날짜 키워드 조회
+  const lowestSlopeDateKeyword = await db.Keyword.findOne({
+    where: { userId, date: lowestSlopeDate },
+  });
+
+  return res.status(200).json({
+    lowestDateDiary,
+    lowestDateKeyword,
+    highestDateDiary,
+    highestDateKeyword,
+    lowestSlopeDateDiary,
+    lowestSlopeDateKeyword,
+  });
 };
 
 module.exports = {
